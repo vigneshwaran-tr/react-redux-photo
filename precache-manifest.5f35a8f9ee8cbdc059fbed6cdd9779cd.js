b91173b0836a091a53152993766a68f5c93febb6
VigneshwaranTR <vignesh.tr@codingmart.com>
2019-01-18 11:26:49 +0530

    Updates

---
+++ precache-manifest.5f35a8f9ee8cbdc059fbed6cdd9779cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cd8279bc97043319f44",
+    "url": "/react-redux-photo/static/js/runtime~main.4cd8279b.js"
+  },
+  {
+    "revision": "642cfb0d412a7a5451b0",
+    "url": "/react-redux-photo/static/js/main.642cfb0d.chunk.js"
+  },
+  {
+    "revision": "894780c3c4d49b0417ab",
+    "url": "/react-redux-photo/static/js/1.894780c3.chunk.js"
+  },
+  {
+    "revision": "642cfb0d412a7a5451b0",
+    "url": "/react-redux-photo/static/css/main.33fa0b40.chunk.css"
+  },
+  {
+    "revision": "894780c3c4d49b0417ab",
+    "url": "/react-redux-photo/static/css/1.83d2c87e.chunk.css"
+  },
+  {
+    "revision": "7315eb61d60b1402e21f3ea107193748",
+    "url": "/react-redux-photo/index.html"
+  }
+];
